aee1495f82397b50084d6282b9a003f8536def00
sangh <jeonsangho1587@gmail.com>
2021-11-06 17:33:38 +0900

    new api added

--- app/routes/ingredients.js
+++ app/routes/ingredients.js
@@ -3,6 +3,6 @@ import * as ingredientController from "../controllers/ingredientController.js";
 
 var router = express.Router();
 
-router.get("/getingred/:name", ingredientController.getIngredient);
+router.get("/ingredient/:name", ingredientController.getIngredient);
 
 export default router;
