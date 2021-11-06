fc0d152b671981adc6a2088c54cbdf55c6adc6cf
sangh <jeonsangho1587@gmail.com>
2021-11-06 17:11:23 +0900

    ingredient finish

--- app/controllers/ingredientController.js
+++ app/controllers/ingredientController.js
@@ -2,13 +2,12 @@ import IngModel from "../models/ingredients.js";
 
 export const getIngredient = async function (req, res) {
   try {
-    const param = '"' + req.params.name.toString() + '"';
+    const param = req.params.name;
     console.log(param);
-    // const data = await IngredientModel.find()
-    //   .where("name")
-    //   .equals("default")
-    //   .select(param);
-    const data = await IngModel.find({ name: "default" });
+    const data = await IngModel.find()
+      .where("name")
+      .equals("test")
+      .select(param);
     console.log(data);
     return res.jsonResult(200, data);
   } catch (err) {
