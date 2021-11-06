a1b019c37b8c60d7f57faca669d9ad50596e58ce
dlxortmd <dlxortmd987@gamil.com>
2021-11-06 14:44:00 +0900

    init

--- app/controllers/foodController.js
+++ app/controllers/foodController.js
@@ -10,7 +10,26 @@ export const getAllFood = async function (req, res) {
   }
 };
 
-export const getSumFoos = async function (req, res) {};
+export const getSumFoos = async function (req, res) {
+  try {
+    const query = req.query.name.split(",");
+
+    const ingredients = await FoodModel.find(
+      {
+        name: { $in: query },
+      },
+      {
+        ingredients: true,
+      }
+    );
+
+    console.log(ingredients);
+    return res.jsonResult(200, { ingredients });
+  } catch (err) {
+    console.log(err);
+    return res.jsonResult(500, { message: "Controller error" });
+  }
+};
 
 export const getRecipy = async function (req, res) {
   try {
