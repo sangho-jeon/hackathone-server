52a8d5021943e001650fdac9cace863bdcd9a94a
sangh <jeonsangho1587@gmail.com>
2021-11-06 16:13:23 +0900

    refactor

--- app/controllers/foodController.js
+++ app/controllers/foodController.js
@@ -2,7 +2,13 @@ import FoodModel from "../models/food.js";
 
 export const getAllFood = async function (req, res) {
   try {
-    const allFoods = await FoodModel.find();
+    const allFoods = await FoodModel.find(
+      {},
+      {
+        name: true,
+        subname: true,
+      }
+    );
     return res.jsonResult(200, allFoods);
   } catch (err) {
     console.log(err);
@@ -34,32 +40,25 @@ export const getSumFoos = async function (req, res) {
 export const getRecipe = async function (req, res) {
   try {
     const params = req.params;
-    const { recipy } = await FoodModel.findOne({ name: params.name });
-    console.log(recipy);
-    return res.jsonResult(200, recipy);
+    const { recipe } = await FoodModel.findOne({ name: params.name });
+    console.log(recipe);
+    return res.jsonResult(200, recipe);
   } catch (err) {
     return res.jsonResult(500, { message: "Controller error" });
   }
 };
 
-export const getNutrition = async function (req, res) {
+export const getFoodData = async function (req, res) {
   try {
     const params = req.params;
-    const { nutrition } = await FoodModel.findOne({ name: params.name });
-    return res.jsonResult(200, nutrition);
-  } catch (err) {
-    return res.jsonResult(500, { message: "Controller error" });
-  }
-};
-
-export const getIngred = async function (req, res) {
-  try {
-    const params = req.params;
-    const { ingredients, sauce } = await FoodModel.findOne({
-      name: params.name,
-    });
-
-    return res.jsonResult(200, { ingredients, sauce });
+    const foodData = await FoodModel.findOne(
+      { name: params.name },
+      {
+        ingredients: true,
+        nutrition: true,
+      }
+    );
+    return res.jsonResult(200, foodData);
   } catch (err) {
     return res.jsonResult(500, { message: "Controller error" });
   }
--- app/routes/food.js
+++ app/routes/food.js
@@ -7,9 +7,8 @@ var router = express.Router();
 router.post("/foodinfo", uploadController.upload);
 router.post("/inginfo", uploadController.uploadIngredients);
 
-router.get("/allfood", foodController.getAllFood);
+router.get("/getallfood", foodController.getAllFood);
 router.get("/getrecipe/:name", foodController.getRecipe);
 router.get("/getfooddata/:name", foodController.getFoodData);
-router.get("/getingred/:name", foodController.getIngred);
 
 export default router;
