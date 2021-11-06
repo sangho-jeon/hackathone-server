65213ef0c006ebd324191f2ac6e3015b06707d39
dlxortmd <dlxortmd987@gamil.com>
2021-11-06 15:54:58 +0900

    conflict

--- app/controllers/foodController.js
+++ app/controllers/foodController.js
@@ -2,13 +2,7 @@ import FoodModel from "../models/food.js";
 
 export const getAllFood = async function (req, res) {
   try {
-    const allFoods = await FoodModel.find(
-      {},
-      {
-        name: true,
-        substring: true,
-      }
-    );
+    const allFoods = await FoodModel.find();
     return res.jsonResult(200, allFoods);
   } catch (err) {
     console.log(err);
@@ -28,9 +22,6 @@ export const getSumFoos = async function (req, res) {
         ingredients: true,
       }
     );
-    var mapFunc = function () {
-      emit(this.name, ingredients);
-    };
 
     console.log(ingredients);
     return res.jsonResult(200, { ingredients });
@@ -43,26 +34,19 @@ export const getSumFoos = async function (req, res) {
 export const getRecipe = async function (req, res) {
   try {
     const params = req.params;
-    const { recipe } = await FoodModel.findOne({ name: params.name });
-    console.log(recipe);
-    return res.jsonResult(200, recipe);
+    const { recipy } = await FoodModel.findOne({ name: params.name });
+    console.log(recipy);
+    return res.jsonResult(200, recipy);
   } catch (err) {
     return res.jsonResult(500, { message: "Controller error" });
   }
 };
 
-export const getFoodData = async function (req, res) {
+export const getNutrition = async function (req, res) {
   try {
     const params = req.params;
-    const foodData = await FoodModel.findOne(
-      { name: params.name },
-      {
-        name: true,
-        ingredient: true,
-        nutrition: true,
-      }
-    );
-    return res.jsonResult(200, foodData);
+    const { nutrition } = await FoodModel.findOne({ name: params.name });
+    return res.jsonResult(200, nutrition);
   } catch (err) {
     return res.jsonResult(500, { message: "Controller error" });
   }
